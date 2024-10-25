9816dc80299a46be7f0bc5ef2b1a51161edb1318
Caldeirafe <felipecaldeira35@gmail.com>
2024-10-25 08:50:27 -0300

    banqueta

---
+++ banco/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Bacon</title>
+</head>
+
+<body>
+<main>
+    
+        <form method="post" class="form">
+            <div class="form-title"><span>Gerenciador</span></div>
+            <div class="title-2"><span>Bacon</span></div>
+            <div class="input-container">
+                <input name="servico" class="input-mail" type="text" placeholder="Serviços">
+                <input name="email" class="input-mail" type="email" placeholder="Email">
+                <span> </span>
+            </div>
+    
+            <section class="bg-stars">
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+                <span class="star"></span>
+            </section>
+    
+            <div class="input-container">
+                <input name="senha" class="input-pwd" type="password" placeholder="Senha">
+            </div>
+            <button type="submit" class="submit">
+                <span class="sign-text">Confirma</span>
+            </button>
+    
+            <p class="signup-link">
+                Não tem conta ?
+                <a href="" class="up">Foda-se!</a>
+            </p>
+    
+        </form>
+</main>
+
+</body>
+
+</html>
